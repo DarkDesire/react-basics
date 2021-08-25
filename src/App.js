79cf16551529786599f4845e3b14917740e9d5f7
Eldar Dragomir <oh.eldarkaa@gmail.com>
2021-08-25 23:23:32 +0300

    inline css#2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './Custom.css'
+
+// https://transform.tools/css-to-js
+const styles = {
+  innerDiv: {
+    textAlign: "center",
+    margin: "auto",
+    width: "60%",
+    border: "3px solid black",
+    backgroundColor: "#73AD21",
+    padding: "70px 0"
+  },
+  outerDiv: { 
+    padding: "70px 0", 
+    backgroundColor: "red" }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello, there is my first app</h1>
-    </div>
-  );
+  return (<div style={styles.outerDiv}>
+    <div style={styles.innerDiv}>This is a green box</div>
+  </div>)
 }
 
 export default App;

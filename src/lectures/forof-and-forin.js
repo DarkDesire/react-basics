48cd024f1eb4aa0d0d582efd07ebdd86897aa967
Eldar Dragomir <oh.eldarkaa@gmail.com>
2021-08-25 23:08:40 +0300

    for iteration

---
+++ src/lectures/forof-and-forin.js
@@ -0,0 +1,15 @@
+const fruitsObject = {
+    apple: 13,
+    orange: 17,
+    banana:15
+}
+// for in - objects
+for (const key in fruitsObject) {
+    console.log(fruitsObject[key])
+}
+// for of - array, string, TypedArray, Map, Set
+const iterable = 'boo';
+
+for (const value of iterable) {
+  console.log(value);
+}

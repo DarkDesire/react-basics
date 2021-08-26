01f13d9f8ca32ec477a6fc1be6f5c62568fc8b1e
Eldar Dragomir <oh.eldarkaa@gmail.com>
2021-08-26 18:03:18 +0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Card from './Card'
 import faker from 'faker'
 
-const buttonsMarkup = (
-  <div>
-    <button className="button button2">Yes</button>
-        <button className="button button3">No</button>
-  </div>
-)
 function App() {
+  const [name, setName] = useState("Alan Smith")
+  const buttonsMarkup = (
+    <div>
+      <button className="button button2">Yes</button>
+      <button className="button button3">No</button>
+    </div>
+  )
+  const changeNameHandler = () => setName(`${faker.name.firstName()} ${faker.name.lastName()}`)
+
   return (
     <div className="App">
+      <button className="button button4" onClick={changeNameHandler}>Change Name</button>
       <Card 
-        avatar={faker.image.avatar()}
-        name={`${faker.name.firstName()} ${faker.name.lastName()}`}
-        title={faker.name.jobTitle()}
+        avatar='https://cdn.fakercloud.com/avatars/nfedoroff_128.jpg'
+        name={name}
+        title="Internal Creative Engineer"
       >{buttonsMarkup}</Card>
-      
-      <Card 
-        avatar={faker.image.avatar()}
-        name={`${faker.name.firstName()} ${faker.name.lastName()}`}
-        title={faker.name.jobTitle()}
-      >{buttonsMarkup}</Card>
-      
-      <Card 
-        avatar={faker.image.avatar()}
-        name={`${faker.name.firstName()} ${faker.name.lastName()}`}
-        title={faker.name.jobTitle()}
-      >{buttonsMarkup}</Card>
-      
     </div>
   );
 }

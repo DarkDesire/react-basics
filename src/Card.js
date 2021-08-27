40d1eb4b5a0a7bda9e4ae653e6e01aac0f722108
Eldar Dragomir <oh.eldarkaa@gmail.com>
2021-08-27 21:08:08 +0300

    Card to Class Component

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const Card = props => {
-    return (
-        <div className="card">
-            <img src={props.avatar} alt="Avatar" style={{width:'100%'}}/>
-            <div className="container">
-            <h4><b>{props.name}</b></h4>
-            <p>{props.title}</p>
-            <input type="text" value={props.name} onChange={props.onChangedName}></input>
-            <p><button className="button button-red" onClick={props.onDelete}>Delete</button></p>
+class Card extends Component {
+    render(){
+        return (
+            <div className="card">
+                <img src={this.props.avatar} alt="Avatar" style={{width:'100%'}}/>
+                <div className="container">
+                <h4><b>{this.props.name}</b></h4>
+                <p>{this.props.title}</p>
+                <input type="text" value={this.props.name} onChange={this.props.onChangedName}></input>
+                <p><button className="button button-red" onClick={this.props.onDelete}>Delete</button></p>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Card

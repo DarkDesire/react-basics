b5a4fdc838e91eaf0497ba867ef70741adba3e43
Eldar Dragomir <oh.eldarkaa@gmail.com>
2021-08-27 23:25:58 +0300

    useContext

--- src/components/ComponentB.js
+++ src/components/ComponentB.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import ComponentC from './ComponentC'
+import {NameContext, ColorContext} from '../ReactContextApp'
 
 const ComponentB = () => {
+    const name = useContext(NameContext)
+    const color = useContext(ColorContext)
     return (
         <div>
             <div>ComponentB</div>
+            <div>Name:{name} | Color:{color}</div>
             <ComponentC/>
         </div>
     )

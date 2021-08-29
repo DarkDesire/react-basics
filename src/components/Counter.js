abefbd70a0229bbacaa928f0ce7ebcc06057516e
Eldar Dragomir <oh.eldarkaa@gmail.com>
2021-08-29 16:07:11 +0300

    Counter done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,53 +3,49 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, ButtonGroup, Badge } from 'reactstrap';
 
 const initialState = {
-    counter1: 0,
-    counter2: 10
+    counter: 0
 }
 const reducer = (state, action) => {
     switch (action.type) {
-        case 'increment1':
-            return {...state, counter1: state.counter1+1+action.payload}
-        case 'decrement1':
-            return {...state, counter1: state.counter1-1-action.payload}
-        case 'increment2':
-            return {...state, counter2: state.counter2+1}
-        case 'decrement2':
-            return {...state, counter2: state.counter2-1}
+        case 'increment':
+            return {...state, counter: state.counter+1}
+        case 'decrement':
+            return {...state, counter: state.counter-1}
         case 'reset':
-            return {...state, counter1: initialState.counter1}
+            return {...state, counter: initialState.counter}
         default:
             return state
     }
 }
 
 const Counter = _ => {
-    const [count, dispatch] = useReducer(reducer, initialState)
+    const [state1, dispatch1] = useReducer(reducer, initialState)
+    const [state2, dispatch2] = useReducer(reducer, initialState)
     return (
         <div>
             <ButtonGroup>
-                <Button color="primary" outline>Counter1 
-                    <Badge color="secondary">{count.counter1} </Badge>
+                <Button color="danger">
+                    Counter1 <Badge>{state1.counter}</Badge>
                 </Button>
-                <Button color="primary" outline>Counter2 
-                    <Badge color="secondary">{count.counter2} </Badge>
+                <Button color="primary">Counter2 
+                    <Badge>{state2.counter}</Badge>
                 </Button>
             </ButtonGroup>
             <p></p>
             <ButtonGroup>
-                <Button color="dark" onClick={() => { dispatch({type:'increment1', payload: 1})}}>increment counter1 1</Button>
-                <Button color="dark" onClick={() => { dispatch({type:'decrement1', payload: 1})}}>decrement counter1 1</Button>
+                <Button color="dark" onClick={() => { dispatch1({type:'increment'})}}>increment</Button>
+                <Button color="dark" onClick={() => { dispatch1({type:'decrement'})}}>decrement</Button>
             </ButtonGroup>
             <p></p>
             <ButtonGroup>
-                <Button color="dark" onClick={() => { dispatch({type:'increment1', payload: 5})}}>increment counter1 5</Button>
-                <Button color="dark" onClick={() => { dispatch({type:'decrement1', payload: 5})}}>decrement counter1 5</Button>
+                <Button color="dark" onClick={() => { dispatch2({type:'increment'})}}>increment</Button>
+                <Button color="dark" onClick={() => { dispatch2({type:'decrement'})}}>decrement</Button>
             </ButtonGroup>
             <p></p>
-            <ButtonGroup>
-                <Button color="dark" onClick={() => { dispatch({type:'increment2', payload: 5})}}>increment counter2</Button>
-                <Button color="dark" onClick={() => { dispatch({type:'decrement2', payload: 5})}}>decrement counter2</Button>
-            </ButtonGroup>
+            <Button color="danger" onClick={() => { 
+                dispatch1({type:'reset'})
+                dispatch2({type:'reset'})
+            }}>reset all</Button>
         </div>
         
     );

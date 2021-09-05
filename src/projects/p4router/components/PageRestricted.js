42c8e5aa1b169d9994788d5bf19abea051adc36c
Eldar Dragomir <oh.eldarkaa@gmail.com>
2021-09-05 23:39:57 +0300

    PageRestricted

---
+++ src/projects/p4router/components/PageRestricted.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PageRestricted = () => {
+    return (
+        <div>
+                <div className="container">
+            <h4 className="center">Page Restricted</h4>
+           
+        </div>
+        </div>
+    )
+}
+
+export default PageRestricted
